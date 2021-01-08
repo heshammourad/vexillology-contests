225483509978e9d02a39f8c6fe015a19b444d374
Hesham Mourad <heshammourad@gmail.com>
2021-01-07 21:57:03 -0500

    Add AppBar

--- react-ui/src/pages/contests/Contests.jsx
+++ react-ui/src/pages/contests/Contests.jsx
@@ -1,3 +1,15 @@
-const Contests = () => <div>Contests</div>;
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
+const Contests = () => (
+  <>
+    <AppBar position="static" color="transparent">
+      <Toolbar>
+        <Typography variant="h6">Vexillology Contests</Typography>
+      </Toolbar>
+    </AppBar>
+  </>
+);
 
 export default Contests;
