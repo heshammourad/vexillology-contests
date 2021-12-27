be655344f6e747f67ed1ae2998fcbac978bc2a45
Hesham Mourad <heshammourad@gmail.com>
2021-12-26 19:25:14 -0800

    Add Nested Lists on contests page

--- react-ui/src/pages/contests/Contests.jsx
+++ react-ui/src/pages/contests/Contests.jsx
@@ -1,38 +1,90 @@
+import Collapse from '@material-ui/core/Collapse';
 import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListSubheader from '@material-ui/core/ListSubheader';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
+import ExpandLess from '@material-ui/icons/ExpandLess';
+import ExpandMore from '@material-ui/icons/ExpandMore';
 import format from 'date-fns/format';
+import getYear from 'date-fns/getYear';
 import parseISO from 'date-fns/parseISO';
+import groupBy from 'lodash/groupBy';
+import { Fragment, useEffect, useState } from 'react';
 
 import { useSwrData } from '../../common';
 import { AppBarDivided, ListItemLink } from '../../components';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
   list: {
     maxWidth: 512,
   },
-});
+  nested: {
+    paddingLeft: theme.spacing(4),
+  },
+}));
 
 const Contests = () => {
   const contests = useSwrData('/contests');
+
+  const [openYear, setOpenYear] = useState(null);
+  useEffect(() => {
+    if (contests) {
+      setOpenYear(getYear(parseISO(contests[0].date)).toString());
+    }
+  }, [contests]);
+
+  const groups = contests ? groupBy(contests, ({ date }) => getYear(parseISO(date))) : null;
+
+  const toggleYearStatus = (year) => {
+    setOpenYear(openYear === year ? null : year);
+  };
+
+  const monthlyContestsSubheaderId = 'monthly-contests-subheader';
+
   const classes = useStyles();
   return (
     <div>
       <AppBarDivided position="static" color="default">
         <Typography variant="h6">Vexillology Contests</Typography>
       </AppBarDivided>
-      {contests && !!contests.length && (
+      {!!groups && (
         <div className={classes.list}>
-          <List>
-            {contests.map(({
-              date, id, name, yearEnd,
-            }) => (
-              <ListItemLink
-                key={id}
-                primary={`${!yearEnd ? `${format(parseISO(date), 'MMM yy')} - ` : ''}${name}`}
-                to={`/contests/${id}`}
-              />
-            ))}
+          <List
+            aria-labelledby={monthlyContestsSubheaderId}
+            subheader={(
+              <ListSubheader component="div" id={monthlyContestsSubheaderId}>
+                Monthly Contests
+              </ListSubheader>
+            )}
+          >
+            {Object.keys(groups)
+              .sort((a, b) => b - a)
+              .map((year) => (
+                <Fragment key={year}>
+                  <ListItem button onClick={() => toggleYearStatus(year)}>
+                    <ListItemText>{year}</ListItemText>
+                    {openYear === year ? <ExpandLess /> : <ExpandMore />}
+                  </ListItem>
+                  <Collapse in={year === openYear} timeout="auto" unmountOnExit>
+                    <List component="div" disablePadding>
+                      {groups[year].map(({
+                        date, id, name, yearEnd,
+                      }) => (
+                        <ListItemLink
+                          key={id}
+                          className={classes.nested}
+                          primary={`${
+                            !yearEnd ? `${format(parseISO(date), 'MMM yy')} - ` : ''
+                          }${name}`}
+                          to={`/contests/${id}`}
+                        />
+                      ))}
+                    </List>
+                  </Collapse>
+                </Fragment>
+              ))}
           </List>
         </div>
       )}
