8020695084832c9ffac9ed4a90638aa6c4516183
Hesham Mourad <heshammourad@gmail.com>
2022-02-06 16:32:45 -0800

    Use localStorage as cache for shared data calls across tabs

--- react-ui/src/common/useSwrData.jsx
+++ react-ui/src/common/useSwrData.jsx
@@ -1,12 +1,30 @@
+import addMinutes from 'date-fns/addMinutes';
+import isPast from 'date-fns/isPast';
 import { useState } from 'react';
 import useSWR from 'swr';
+import createPersistedState from 'use-persisted-state';
+
+const usePersistentState = createPersistedState('data');
+const useExpiresState = createPersistedState('expires');
 
 const useSwrData = (key) => {
   const { data, mutate } = useSWR(key);
   const [isFetched, setFetched] = useState(!!data);
+  const [cache, setCache] = usePersistentState({});
+  const [expires, setExpires] = useExpiresState({});
 
   if (!isFetched) {
-    setTimeout(mutate, 200);
+    setTimeout(async () => {
+      const expiryDate = expires[key];
+      if (expiryDate && !isPast(new Date(expiryDate))) {
+        mutate(cache[key], false);
+        return;
+      }
+
+      const response = await mutate();
+      setCache({ ...cache, [key]: response });
+      setExpires({ ...expires, [key]: addMinutes(new Date(), 30) });
+    }, 200);
     setFetched(true);
   }
 
