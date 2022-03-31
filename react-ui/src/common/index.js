b4e39cb06dd14c7a7014228399991e2301abb5e9
Hesham Mourad <heshammourad@gmail.com>
2022-03-30 22:35:08 -0700

    Log page_view event

--- react-ui/src/common/index.js
+++ react-ui/src/common/index.js
@@ -1,3 +1,4 @@
 export { default as DrawerStateContext } from './DrawerStateContext';
+export { default as analytics } from './firebase';
 export { default as useClientWidth } from './useClientWidth';
 export { default as useSwrData } from './useSwrData';
