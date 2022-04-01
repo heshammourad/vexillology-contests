2deac9250a3ddddece491835ae98a5ffe1c17d6a
Hesham Mourad <heshammourad@gmail.com>
2022-03-31 23:54:11 -0700

    Fix winners not showing up on Entry page

--- react-ui/src/pages/entry/Entry.jsx
+++ react-ui/src/pages/entry/Entry.jsx
@@ -56,8 +56,8 @@ const useStyles = makeStyles((theme) => ({
 
 export default function PersistentDrawerRight() {
   const { contestId, entryId } = useParams();
-  const { entries = [] } = useSwrData(`/contests/${contestId}`) || {};
-  const entry = entries.find(({ id }) => id === entryId) || {};
+  const { entries = [], winners = [] } = useSwrData(`/contests/${contestId}`) || {};
+  const entry = [...entries, ...winners].find(({ id }) => id === entryId) || {};
 
   const classes = useStyles();
 
