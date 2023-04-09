81d1fa45597d6bb7f9742d087bec9d52f01a1f61
Hesham Mourad <heshammourad@gmail.com>
2023-04-08 22:26:01 -0700

    Add submissions to /init

--- server/api/init.js
+++ server/api/init.js
@@ -1,19 +1,34 @@
 const db = require('../db');
+const { getCurrentContestSubmissions } = require('../db/queries');
 const { createLogger } = require('../logger');
+const reddit = require('../reddit');
 
 const { TITLE = 'Vexillology Contests', WEB_APP_CLIENT_ID } = process.env;
 
 const logger = createLogger('API/INIT');
 
-exports.get = async (req, res) => {
+exports.get = async ({ headers: { accesstoken, refreshtoken } }, res) => {
   try {
     const experimentsData = await db.select('SELECT * FROM experiments');
     const experiments = experimentsData.reduce((acc, { active, name }) => {
       acc[name] = active;
       return acc;
     }, {});
+
+    let isModerator = false;
+    let submissionsToReview = 0;
+    if (accesstoken && refreshtoken) {
+      isModerator = await reddit.isModerator({ accesstoken, refreshtoken });
+      if (isModerator) {
+        const submissions = await getCurrentContestSubmissions();
+        submissionsToReview = submissions.length;
+      }
+    }
+
     res.send({
       experiments,
+      isModerator,
+      submissionsToReview,
       title: TITLE,
       webAppClientId: WEB_APP_CLIENT_ID,
     });
--- server/db/queries.js
+++ server/db/queries.js
@@ -8,6 +8,28 @@ exports.getCategories = async (contestId) => {
   return categories.map(({ category }) => category);
 };
 
+exports.getCurrentContest = async () => {
+  const [result] = await db.select(
+    `SELECT id, name, prompt, submission_start, submission_end, now()
+     FROM contests
+     WHERE submission_start < now()
+     ORDER BY submission_start DESC
+     LIMIT 1`,
+  );
+  return result;
+};
+
+exports.getCurrentContestSubmissions = async () => {
+  const { id } = await this.getCurrentContest();
+  const submissions = await db.select(
+    `SELECT ce.category, e.description, e.id, e.name, e.submission_status, e.url, e.user
+     FROM contest_entries ce, entries e
+     WHERE ce.entry_id = e.id AND ce.contest_id = $1`,
+    [id],
+  );
+  return submissions;
+};
+
 exports.getVoteDates = async (contestId) => {
   const voteDates = await db.select(
     'SELECT vote_start, vote_end, now() FROM contests WHERE id = $1',
