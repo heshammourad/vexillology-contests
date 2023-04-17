022f96cbcbecc8d9c13b0e23ba286a3ba6d98d1b
Hesham Mourad <heshammourad@gmail.com>
2023-04-16 19:25:10 -0700

    Add row actions to review submissions table

--- server/api/reviewSubmissions.js
+++ server/api/reviewSubmissions.js
@@ -8,17 +8,25 @@ exports.get = async (req, res) => {
     const { name, submissionEnd } = await getCurrentContest();
     const submissions = await getCurrentContestSubmissions();
 
-    const submissionMap = new Map();
-    submissions.forEach(({ user }) => {
-      const currentCount = submissionMap.get(user);
-      submissionMap.set(user, 1 + currentCount || 0);
+    const breakdownMap = new Map();
+    submissions.forEach(({ submissionStatus, user }) => {
+      const userBreakdown = breakdownMap.get(user) ?? {};
+      const submittedCount = userBreakdown?.submitted ?? 0;
+      userBreakdown.submitted = submittedCount + 1;
+
+      if (submissionStatus === 'approved') {
+        const approvedCount = userBreakdown?.approved ?? 0;
+        userBreakdown.approved = approvedCount + 1;
+      }
+
+      breakdownMap.set(user, userBreakdown);
     });
 
     res.status(200).send({
       name,
       submissionEnd,
-      submissionCountByUser: Object.fromEntries(submissionMap),
       submissions,
+      userBreakdown: Object.fromEntries(breakdownMap),
     });
   } catch (err) {
     logger.error(`Error getting /reviewSubmissions: ${err}`);
