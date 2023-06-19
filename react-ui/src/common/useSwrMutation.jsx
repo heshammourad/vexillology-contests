17355efab35dd00376d303d56a5963be174c56b7
Hesham Mourad <heshammourad@gmail.com>
2023-06-18 23:25:39 -0700

    Fix voting mutation error
    Updated to properly handle case where the API to update the data differs from the key to mutate

--- react-ui/src/common/useSwrMutation.jsx
+++ react-ui/src/common/useSwrMutation.jsx
@@ -15,20 +15,21 @@ import useAuthState from './useAuthState';
  * React hook used for updating data on the server and mutating the local SWR data.
  * @see {@link https://swr.vercel.app/docs/mutation#useswrmutation|useSWRMutation}
  *
- * @param {string} path - Path of the API to call.
+ * @param {string} key - Key of the API to mutate.
  * @param {apiFunction} apiFunction - Function to call API with, e.g. post/put.
+ * @param {string} [apiPath] - Path to the mutating API function. `path` if not provided.
  * @returns {*} data - data for the given key returned from `fetcher`
  * @returns {*} error - error thrown by `fetcher` (or undefined)
  * @returns {*} trigger(arg, options) - a function to trigger a remote mutation
  * @returns {*} reset - a function to reset the state (`data`, `error`, `isMutating`)
  * @returns {*} isMutating - if there's an ongoing mutation
  */
-const useSwrMutation = (path, apiFunction) => {
+const useSwrMutation = (key, apiFunction, apiPath) => {
   const [{ accessToken, refreshToken }] = useAuthState();
   const authTokens = { accessToken, refreshToken };
 
   // eslint-disable-next-line max-len
-  const response = useSWRMutation([path, authTokens], (_, { arg }) => apiFunction(path, arg, authTokens));
+  const response = useSWRMutation([key, authTokens], (_, { arg }) => apiFunction(apiPath ?? key, arg, authTokens));
 
   return { ...response, data: response.data || {} };
 };
