34b07a0d1a19c68dbf8f3056747e10a8fc6b469d
Hesham Mourad <heshammourad@gmail.com>
2024-01-02 22:11:01 -0800

    Wrap setAuthState in try-catch

--- react-ui/src/common/useAuthState.jsx
+++ react-ui/src/common/useAuthState.jsx
@@ -10,7 +10,12 @@ const useAuth = () => {
   // Temp refactoring, to be replaced by force logout in the future
   const { accessToken, authTokens = {}, refreshToken } = authState;
   if (Object.keys(authTokens).length === 0 && accessToken && refreshToken) {
-    setAuthState({ ...authState, authTokens: { accessToken, refreshToken } });
+    try {
+      setAuthState({ ...authState, authTokens: { accessToken, refreshToken } });
+    } catch (e) {
+      // setAuthState is throwing a null reference error, but still updating,
+      // so wrapping in a try-catch. This should be fixed by replacing this library in #281.
+    }
   }
 
   return [authState, setAuthState];
