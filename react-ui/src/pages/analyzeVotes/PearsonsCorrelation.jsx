cebddd53891e165aaec4d70672313ef581079931
Hesham Mourad <heshammourad@gmail.com>
2024-08-05 18:23:40 -0700

    Fix multiple instances of logger

--- react-ui/src/pages/analyzeVotes/PearsonsCorrelation.jsx
+++ react-ui/src/pages/analyzeVotes/PearsonsCorrelation.jsx
@@ -1,12 +1,9 @@
-/* eslint-disable max-len */
 /* eslint-disable react/forbid-prop-types */
 import PropTypes from 'prop-types';
 import { useCallback, useEffect, useMemo } from 'react';
 import Plot from 'react-plotly.js';
 
-import {
-  roundTwoDecimals, createTraces, trimUsername,
-} from './functions';
+import { roundTwoDecimals, createTraces, trimUsername } from './functions';
 import MARKERS from './markers';
 
 const GROUP = { selected: 0, none: 1, few: 2 };
@@ -15,7 +12,12 @@ const GROUP = { selected: 0, none: 1, few: 2 };
  * Determine similarity between selected user and all other users
  */
 function PearsonsCorrelation({
-  user1, user2, votes, entryPositionLookup, setUser2, voteMinimum,
+  user1,
+  user2,
+  votes,
+  entryPositionLookup,
+  setUser2,
+  voteMinimum,
 }) {
   /**
    * CALCULATE PEARSONS
@@ -23,7 +25,9 @@ function PearsonsCorrelation({
    * covariance(x, y) / (sd_x * sd_y)
    */
   const pearsons = useMemo(() => {
-    if (!user1) { return []; }
+    if (!user1) {
+      return [];
+    }
 
     const numberOfEntries = Object.keys(entryPositionLookup).length;
     const allVotesByUser = votes.reduce((acc, vote) => {
@@ -36,36 +40,50 @@ function PearsonsCorrelation({
 
     const userVotes = allVotesByUser[user1];
     // User did not vote
-    if (!userVotes) { return []; }
+    if (!userVotes) {
+      return [];
+    }
 
     const validPearsons = [];
 
-    Object.keys(allVotesByUser).filter((u) => u !== user1).forEach((u) => {
-      const otherUser = allVotesByUser[u];
-      let sum1 = 0; let sum2 = 0; let sum1Sq = 0; let sum2Sq = 0; let sumProduct = 0;
-      let n = 0;
-      let numVotes = 0;
-      otherUser.forEach((vote, index) => {
-        const userVote = userVotes[index];
-        if (vote > -1) {
-          numVotes += 1;
-          if (userVote > -1) {
-            sum1 += vote;
-            sum2 += userVote;
-            sum1Sq += vote * vote;
-            sum2Sq += userVote * userVote;
-            sumProduct += vote * userVote;
-            n += 1;
+    Object.keys(allVotesByUser)
+      .filter((u) => u !== user1)
+      .forEach((u) => {
+        const otherUser = allVotesByUser[u];
+        let sum1 = 0;
+        let sum2 = 0;
+        let sum1Sq = 0;
+        let sum2Sq = 0;
+        let sumProduct = 0;
+        let n = 0;
+        let numVotes = 0;
+        otherUser.forEach((vote, index) => {
+          const userVote = userVotes[index];
+          if (vote > -1) {
+            numVotes += 1;
+            if (userVote > -1) {
+              sum1 += vote;
+              sum2 += userVote;
+              sum1Sq += vote * vote;
+              sum2Sq += userVote * userVote;
+              sumProduct += vote * userVote;
+              n += 1;
+            }
           }
+        });
+        const num = sumProduct - (sum1 * sum2) / n;
+        const den = Math.sqrt(
+          (sum1Sq - (sum1 * sum1) / n) * (sum2Sq - (sum2 * sum2) / n),
+        );
+
+        if (n !== 0 && den !== 0) {
+          validPearsons.push({
+            username: u,
+            correlation: num / den,
+            group: numVotes < voteMinimum ? GROUP.few : undefined,
+          });
         }
       });
-      const num = sumProduct - ((sum1 * sum2) / n);
-      const den = Math.sqrt((sum1Sq - ((sum1 * sum1) / n)) * (sum2Sq - ((sum2 * sum2) / n)));
-
-      if (n !== 0 && den !== 0) {
-        validPearsons.push({ username: u, correlation: num / den, group: numVotes < voteMinimum ? GROUP.few : undefined });
-      }
-    });
 
     return validPearsons.sort((a, b) => a.correlation - b.correlation);
   }, [user1, votes, entryPositionLookup, voteMinimum]);
@@ -77,8 +95,10 @@ function PearsonsCorrelation({
     x: i,
     y: p.correlation,
     id: p.username,
-    group: p.username === user2 ? GROUP.selected : (p.group || GROUP.none),
-    text: `User 2: ${trimUsername(p.username)}<br />Pearsons: ${roundTwoDecimals(p.correlation)}`,
+    group: p.username === user2 ? GROUP.selected : p.group || GROUP.none,
+    text: `User 2: ${trimUsername(
+      p.username,
+    )}<br />Pearsons: ${roundTwoDecimals(p.correlation)}`,
   }));
 
   /**
@@ -100,19 +120,22 @@ function PearsonsCorrelation({
    * ALLOW KEYED NAVIGATION OF THIS CHART (up-down arrows)
    */
   const usernames = useMemo(() => pearsons.map((p) => p.username), [pearsons]);
-  const handleKeyDown = useCallback((event) => {
-    const { key } = event;
-    if (key === 'ArrowUp' || key === 'ArrowDown') {
-      event.preventDefault();
-      setUser2((prev) => {
-        const index = usernames.indexOf(prev);
-        if (key === 'ArrowUp') {
-          return usernames[index + 1] || usernames[0];
-        }
-        return usernames[index - 1] || usernames[usernames.length - 1];
-      });
-    }
-  }, [setUser2, usernames]);
+  const handleKeyDown = useCallback(
+    (event) => {
+      const { key } = event;
+      if (key === 'ArrowUp' || key === 'ArrowDown') {
+        event.preventDefault();
+        setUser2((prev) => {
+          const index = usernames.indexOf(prev);
+          if (key === 'ArrowUp') {
+            return usernames[index + 1] || usernames[0];
+          }
+          return usernames[index - 1] || usernames[usernames.length - 1];
+        });
+      }
+    },
+    [setUser2, usernames],
+  );
 
   useEffect(() => {
     window.addEventListener('keydown', handleKeyDown);
