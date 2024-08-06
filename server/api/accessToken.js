c6377eff787c4cd2d17a2017483cab855dead83b
Hesham Mourad <heshammourad@gmail.com>
2024-08-05 22:55:53 -0700

    Test

--- server/api/accessToken.js
+++ server/api/accessToken.js
@@ -9,9 +9,13 @@ const { camelizeObjectKeys } = require('../util');
 const logger = createLogger('API/ACCESS_TOKEN');
 
 exports.get = async ({ params: { code } }, res) => {
-  try {    const result = await reddit.retrieveAccessToken(code);  
-       if (!result) {       throw new Error('Unable to retrieve access token');     }
-    camelizeObjectKeys([result]);    const { accessToken, refreshToken } = result;
+  try {
+    const result = await reddit.retrieveAccessToken(code);
+    if (!result) {
+      throw new Error('Unable to retrieve access token');
+    }
+    camelizeObjectKeys([result]);
+    const { accessToken, refreshToken } = result;
     if (!accessToken || !refreshToken) {
       throw new Error('Missing auth tokens');
     }
@@ -19,6 +23,8 @@ exports.get = async ({ params: { code } }, res) => {
     const username = await reddit.getUser(result);
     const response = { accessToken, refreshToken, username };
     res.send(response);
-  } catch (err) {     logger.error(`Error retrieving access token: ${err}`);    res.status(500).send();
+  } catch (err) {
+    logger.error(`Error retrieving access token: ${err}`);
+    res.status(500).send();
   }
 };
