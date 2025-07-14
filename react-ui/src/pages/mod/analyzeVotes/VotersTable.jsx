1cc879373c444861cde6ab6f933e116b82832f58
rissois <44072214+rissois@users.noreply.github.com>
2025-07-13 17:56:26 -0700

    Updated VotersTable to resemble EntrantVotersTable

--- react-ui/src/pages/mod/analyzeVotes/VotersTable.jsx
+++ react-ui/src/pages/mod/analyzeVotes/VotersTable.jsx
@@ -1,3 +1,6 @@
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+import Collapse from '@mui/material/Collapse';
 import Paper from '@mui/material/Paper';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
@@ -6,6 +9,7 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import TableSortLabel from '@mui/material/TableSortLabel';
+import PropTypes from 'prop-types';
 import { useMemo, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 
@@ -14,38 +18,12 @@ import SectionTitleWithButtons from './SectionTitleWithButtons';
 import TableBodyWrapper from './TableBodyWrapper';
 import {
   BanStatusTableText,
-  BlackTableText,
-  OrangeTableText,
-  RedTableText,
+  ScoreTableText,
   TableTextWrapper,
   VoteStatusTableText,
 } from './TableText';
-
-const KARMA_THRESHOLD = 20;
-const VOTED_THRESHOLD = 20;
-
-// Helper function to format age from days
-const formatAge = (ageInDays) => {
-  if (ageInDays < 30) {
-    return `${Math.round(ageInDays)} days`;
-  }
-  if (ageInDays < 365) {
-    const months = Math.round(ageInDays / 30);
-    return `${months} mo`;
-  }
-  return '>1 yr';
-};
-
-// Helper function to format karma
-const formatKarma = (karma) => {
-  if (karma > 1000) {
-    return '>1000';
-  }
-  if (!karma) {
-    return 'unk';
-  }
-  return karma.toString();
-};
+import { VoterBreakdownText } from './VoteBreakdownText';
+import VoterGraph from './VoterGraph';
 
 // Sorting function
 const sortData = (data, sortField, sortDirection) => [...data].sort((a, b) => {
@@ -53,21 +31,13 @@ const sortData = (data, sortField, sortDirection) => [...data].sort((a, b) => {
   let bValue;
 
   switch (sortField) {
-    case 'age':
-      aValue = parseFloat(a.ageInDays);
-      bValue = parseFloat(b.ageInDays);
-      break;
-    case 'karma':
-      aValue = a.karma;
-      bValue = b.karma;
-      break;
-    case 'votePercentage':
-      aValue = parseFloat(a.votePercentage);
-      bValue = parseFloat(b.votePercentage);
-      break;
     case 'username':
       aValue = a.username.toLowerCase();
       bValue = b.username.toLowerCase();
+      break;
+    case 'distrustScore':
+      aValue = a.distrustScore || 0;
+      bValue = b.distrustScore || 0;
       break;
     default:
       return 0;
@@ -82,25 +52,11 @@ const sortData = (data, sortField, sortDirection) => [...data].sort((a, b) => {
 function VotersTable() {
   const navigate = useNavigate();
   const { contestId } = useParams();
-  const [sortField, setSortField] = useState('username');
-  const [sortDirection, setSortDirection] = useState('asc');
+  const [sortField, setSortField] = useState('distrustScore');
+  const [sortDirection, setSortDirection] = useState('desc');
   // Use the contest context for all data
   const {
-    votersData,
-    votersError,
-    votersLoading,
-
-    numberOfEntries,
-    entriesError,
-    entriesLoading,
-
-    voterPatternsData,
-    voterPatternsError,
-    voterPatternsLoading,
-
-    bansData,
-    bansError,
-    bansLoading,
+    votersData, votersError, votersLoading, distrustScores,
   } = useContestContext();
 
   const buttons = useMemo(
@@ -113,12 +69,6 @@ function VotersTable() {
     ],
     [navigate, contestId],
   );
-
-  const handleVoterSelection = (v) => {
-    // TODO: Implement voter selection functionality
-    // eslint-disable-next-line no-unused-vars
-    const selectedVoter = v;
-  };
 
   const handleSort = (field) => {
     if (sortField === field) {
@@ -135,18 +85,12 @@ function VotersTable() {
       Object.entries(votersData).map(([username, values]) => ({
         username,
         ...values,
-        votePercentage:
-            voterPatternsData && numberOfEntries
-              ? Math.round(
-                (100 * (voterPatternsData[username]?.voteCount || 0))
-                    / numberOfEntries,
-              )
-              : 0,
+        distrustScore: distrustScores[username]?.score || 0,
       })),
       sortField,
       sortDirection,
     ),
-    [votersData, numberOfEntries, voterPatternsData, sortField, sortDirection],
+    [votersData, distrustScores, sortField, sortDirection],
   );
 
   return (
@@ -166,37 +110,19 @@ function VotersTable() {
                   Voter
                 </TableSortLabel>
               </TableCell>
-              <TableCell align="center">
-                <TableSortLabel
-                  active={sortField === 'age'}
-                  direction={sortField === 'age' ? sortDirection : 'asc'}
-                  onClick={() => handleSort('age')}
-                >
-                  Age
-                </TableSortLabel>
-              </TableCell>
-              <TableCell align="center">
-                <TableSortLabel
-                  active={sortField === 'karma'}
-                  direction={sortField === 'karma' ? sortDirection : 'asc'}
-                  onClick={() => handleSort('karma')}
-                >
-                  Karma
-                </TableSortLabel>
-              </TableCell>
-              <TableCell align="center">
-                <TableSortLabel
-                  active={sortField === 'votePercentage'}
-                  direction={
-                    sortField === 'votePercentage' ? sortDirection : 'asc'
-                  }
-                  onClick={() => handleSort('votePercentage')}
-                >
-                  % voted
-                </TableSortLabel>
-              </TableCell>
               <TableCell align="center">Vote status</TableCell>
               <TableCell align="center">Ban status</TableCell>
+              <TableCell align="center">
+                <TableSortLabel
+                  active={sortField === 'distrustScore'}
+                  direction={
+                    sortField === 'distrustScore' ? sortDirection : 'asc'
+                  }
+                  onClick={() => handleSort('distrustScore')}
+                >
+                  Voter distrust
+                </TableSortLabel>
+              </TableCell>
             </TableRow>
           </TableHead>
           <TableBodyWrapper
@@ -206,64 +132,11 @@ function VotersTable() {
           >
             <TableBody>
               {sortedVotersData.map((voter) => (
-                <TableRow
+                <VoterRow
                   key={voter.username}
-                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                  onClick={() => handleVoterSelection(voter.username)}
-                  hover
-                >
-                  <TableCell component="th" scope="row">
-                    {voter.username}
-                  </TableCell>
-
-                  {
-                    // eslint-disable-next-line no-nested-ternary
-                    voter.ageInDays < 30 ? (
-                      <RedTableText>{formatAge(voter.ageInDays)}</RedTableText>
-                    ) : voter.ageInDays < 365 ? (
-                      <OrangeTableText>
-                        {formatAge(voter.ageInDays)}
-                      </OrangeTableText>
-                    ) : (
-                      <BlackTableText bold center>
-                        {formatAge(voter.ageInDays)}
-                      </BlackTableText>
-                    )
-                  }
-
-                  {voter.karma < KARMA_THRESHOLD ? (
-                    <RedTableText>{formatKarma(voter.karma)}</RedTableText>
-                  ) : (
-                    <BlackTableText bold center>
-                      {formatKarma(voter.karma)}
-                    </BlackTableText>
-                  )}
-
-                  <TableTextWrapper
-                    loading={entriesLoading || voterPatternsLoading}
-                    error={entriesError || voterPatternsError}
-                  >
-                    {voter.votePercentage < VOTED_THRESHOLD ? (
-                      <RedTableText>
-                        {voter.votePercentage}
-                        %
-                      </RedTableText>
-                    ) : (
-                      <BlackTableText bold center>
-                        {voter.votePercentage}
-                        %
-                      </BlackTableText>
-                    )}
-                  </TableTextWrapper>
-
-                  <VoteStatusTableText
-                    voteStatus={votersData[voter.username]?.dq}
-                  />
-
-                  <TableTextWrapper loading={bansLoading} error={bansError}>
-                    <BanStatusTableText banStatus={bansData[voter.username]} />
-                  </TableTextWrapper>
-                </TableRow>
+                  username={voter.username}
+                  voteStatus={voter.dq}
+                />
               ))}
             </TableBody>
           </TableBodyWrapper>
@@ -272,5 +145,138 @@ function VotersTable() {
     </>
   );
 }
+
+function VoterRow({ username, voteStatus }) {
+  const {
+    bansData,
+    bansError,
+    bansLoading,
+    distrustScores,
+    distrustScoresError,
+    distrustScoresLoading,
+  } = useContestContext();
+
+  const [showVoter, setShowVoter] = useState(false);
+
+  return (
+    <>
+      <TableRow
+        sx={{
+          '&:last-child td, &:last-child th': { border: 0 },
+          '& > *': { borderBottom: 'unset' },
+        }}
+        onClick={() => setShowVoter((prev) => !prev)}
+        hover
+      >
+        <TableCell component="th" scope="row">
+          {username}
+        </TableCell>
+
+        <TableTextWrapper loading={false} error={false}>
+          <VoteStatusTableText voteStatus={voteStatus} />
+        </TableTextWrapper>
+
+        <TableTextWrapper loading={bansLoading} error={bansError}>
+          <BanStatusTableText banStatus={bansData[username]} />
+        </TableTextWrapper>
+
+        <TableTextWrapper
+          loading={distrustScoresLoading}
+          error={distrustScoresError}
+        >
+          <ScoreTableText>{distrustScores[username]?.score}</ScoreTableText>
+        </TableTextWrapper>
+      </TableRow>
+      <TableRow onClick={() => setShowVoter(false)}>
+        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={4}>
+          <Collapse in={showVoter} timeout="auto" unmountOnExit>
+            <Box
+              sx={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+              }}
+            >
+              <VoterGraph voterId={username} />
+            </Box>
+            <Box
+              sx={{
+                display: 'flex',
+                justifyContent: 'center',
+                marginTop: 6,
+                marginBottom: 6,
+              }}
+            >
+              <Box
+                sx={{
+                  flex: '1 1 100%',
+                  maxWidth: '600px',
+                  textAlign: 'center',
+                }}
+              >
+                <Typography variant="h6" sx={{ marginBottom: 2 }}>
+                  Voter Distrust Analysis
+                </Typography>
+                {distrustScores[username] ? (
+                  <>
+                    <VoterBreakdownText
+                      text={`Karma: ${distrustScores[username].karmaText}`}
+                      score={Math.round(
+                        distrustScores[username].karmaDistrust * 100,
+                      )}
+                    />
+                    <VoterBreakdownText
+                      text={`Account age: ${distrustScores[username].ageText}`}
+                      score={Math.round(
+                        distrustScores[username].ageDistrust * 100,
+                      )}
+                    />
+                    <VoterBreakdownText
+                      text={`Vote participation: ${distrustScores[username].percentVotedText}`}
+                      score={Math.round(
+                        distrustScores[username].percentVotedDistrust * 100,
+                      )}
+                    />
+                    <VoterBreakdownText
+                      text={`Time between votes: ${distrustScores[username].timeBetweenVotesText}`}
+                      score={Math.round(
+                        distrustScores[username].timeBetweenVotesDistrust * 100,
+                      )}
+                    />
+                    <VoterBreakdownText
+                      text={`Zero ratings: ${distrustScores[username].ratedZerosText}`}
+                      score={Math.round(
+                        distrustScores[username].ratedZerosDistrust * 100,
+                      )}
+                    />
+                    <VoterBreakdownText
+                      text={`Randomness: ${distrustScores[username].randomnessText}`}
+                      score={Math.round(
+                        distrustScores[username].randomnessDistrust * 100,
+                      )}
+                    />
+                  </>
+                ) : (
+                  <Typography>
+                    No distrust data available for this voter
+                  </Typography>
+                )}
+              </Box>
+            </Box>
+          </Collapse>
+        </TableCell>
+      </TableRow>
+    </>
+  );
+}
+
+VoterRow.propTypes = {
+  username: PropTypes.string.isRequired,
+  voteStatus: PropTypes.string,
+};
+
+VoterRow.defaultProps = {
+  voteStatus: undefined,
+};
 
 export default VotersTable;
