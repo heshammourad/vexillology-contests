cc346e3e879c23b04c9049ba4826c0fe8862c4be
rissois <44072214+rissois@users.noreply.github.com>
2025-09-27 15:16:55 -0700

    Updated FMP link for new marketing campaign

--- react-ui/src/components/Fmp.jsx
+++ react-ui/src/components/Fmp.jsx
@@ -6,7 +6,7 @@ import SvgIcon from '@material-ui/core/SvgIcon';
 
 import { ReactComponent as FmpLogo } from '../images/FMP.svg';
 
-const FMP_LINK = 'https://flagmaker-print.com/?utm_source=https%3A%2F%2Fwww.reddit.com%2Fr%2Fvexillology%2F&utm_medium=link&utm_campaign=r_vexilology&utm_id=R+%2F+Vexilology+Subreddit+Partnership&utm_term=flag+maker%2C+custom+flags%2C+create+flag%2C+flag+designer%2C+vexillology';
+const FMP_LINK = 'https://flagmaker-print.com/s/1cc47b';
 
 const FMP_TEXT = 'Powered by Flagmaker & Print ~ the easiest way to design and print custom flags';
 
