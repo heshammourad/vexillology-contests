18abcfb2fa41b93ddacae6f1cd4d64752674dbc3
Hesham Mourad <heshammourad@gmail.com>
2026-01-03 23:54:28 +0000

    Allow contest summary to show previous contest & add copy results button

--- server/api/contestSummary.js
+++ server/api/contestSummary.js
@@ -1,7 +1,7 @@
 const { isFuture } = require('date-fns');
 
 const db = require('../db');
-const { getCurrentContest, getCategories } = require('../db/queries');
+const { getCategories } = require('../db/queries');
 const { createLogger } = require('../logger');
 const { generateImagePath } = require('../util');
 
@@ -10,7 +10,16 @@ const logger = createLogger('API/CONTEST_SUMMARY');
 exports.get = async (req, res) => {
   logger.debug('Getting /contestSummary');
   try {
-    const contest = await getCurrentContest();
+    const contest = await db.select(
+      `
+      SELECT id, results_certified, vote_end
+      FROM contests
+      WHERE vote_start < now()
+      ORDER BY vote_start DESC
+      LIMIT 1`,
+      undefined,
+      true,
+    );
     if (!contest) {
       res.status(404).send();
       return;
@@ -20,7 +29,8 @@ exports.get = async (req, res) => {
     const categories = await getCategories(id);
 
     const contestResults = await db.select(
-      'SELECT * FROM get_contest_results()',
+      'SELECT * FROM get_contest_results($1)',
+      [id],
     );
     if (!contestResults) {
       res.status(404).send();
@@ -37,12 +47,37 @@ exports.get = async (req, res) => {
       ...entry,
       imagePath: generateImagePath(entry.entryId),
     }));
+    const hasCategories = categories.length > 0;
+    const resultsToInclude = entries.filter(
+      (e) => e.rank <= 20 || e.categoryRank === 1,
+    );
+
+    const header = hasCategories
+      ? '|Rank|Username|Submission|Score|Category\n|-:|-|-|-:-'
+      : '|Rank|Username|Submission|Score\n|-:|-|-|-:';
+
+    const rows = resultsToInclude.map((entry) => {
+      const username = `/u/${entry.submitter}`;
+      const submission = `[${entry.entryName}](https://www.vexillologycontests.com${entry.imagePath})`;
+
+      let row = `${entry.rank}|${username}|${submission}|${entry.average}`;
+      if (hasCategories) {
+        row += '|';
+        if (entry.categoryRank === 1) {
+          row += entry.category;
+        }
+      }
+
+      return row;
+    });
+    const resultsMarkdown = [header, ...rows].join('\n');
 
     const response = {
       categories,
       contestId: id,
       entries,
       resultsCertified,
+      resultsMarkdown,
       voteEnd,
     };
     logger.debug(`Sending '${JSON.stringify(response)}' for /contestSummary`);
